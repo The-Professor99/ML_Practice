c3b755a9f729578e14093f45344926b702122d4a
The-Professor99 <festusihechi99@gmail.com>
2024-06-10 19:58:39 +0100

    Add streamlit files for streamlit sample deployment

---
+++ Recommendation/streamlit_recommendation/main_page.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.markdown('''# Welcome
+
+This is a simple recommendation system. It is built on the concepts of collaborative filtering. \
+By default, the algorithm has been trained on a movies dataset to recommend movies but you can further apply it to other datasets provided the datasets meet a particular rule.
+
+''')
